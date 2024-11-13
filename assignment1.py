0b22c451fe6a6fa499910308ce6c24f81557010a
Hassnain Mohammad <hmohammad7@myseneca.ca>
2024-11-13 18:33:49 -0500

    completed the after_function

--- assignment1.py
+++ assignment1.py
@@ -71,36 +71,37 @@ def after(date: str) -> str:
     Return the date for the next day of the given date in DD/MM/YYYY format.
     This function has been tested to work for year after 1582
     '''
+#Next day
     day, mon, year = (int(x) for x in date.split('/'))
-    day += 1  # next day
 
-    lyear = year % 4
-    if lyear == 0:
-        leap_flag = True
-    else:
-        leap_flag = False  # this is not a leap year
+    day += 1
 
-    lyear = year % 100
-    if lyear == 0:
-        leap_flag = False  # this is not a leap year
-
-    lyear = year % 400
-    if lyear == 0:
-        leap_flag = True  # this is a leap year
+#To check leap year
+    leap_flag = leap_year(year)
 
     mon_dict= {1: 31, 2: 28, 3: 31, 4: 30, 5: 31, 6: 30,
            7: 31, 8: 31, 9: 30, 10: 31, 11: 30, 12: 31}
+
     if mon == 2 and leap_flag:
+
         mon_max = 29
+
     else:
+
         mon_max = mon_dict[mon]
 
     if day > mon_max:
+
         mon += 1
+
         if mon > 12:
+
             year += 1
+
             mon = 1
-        day = 1  # if tmp_day > this month's max, reset to 1
+
+        day = 1  #If tmp_day > this month's max, reset to 1
+
     return f"{day:02}/{mon:02}/{year}"
 
 
